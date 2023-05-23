9479d8d71574aad6d7f7b3d915e4c7a2fc165493
User <User@DESKTOP-DRJMU61>
2023-05-23 18:34:07 +0400

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static int count = 1;
+        private static readonly List<User> users = new List<User>();
+        [HttpPost("created")]
+        public string Create(string Name, string Surname)
+        {
+            User user = new User()
+            {
+                Id = count++,
+                Username = Name,
+                Fullname = Surname,
+
+
+            };
+            users.Add(user);
+            return $"\"{Name},{Surname}\"created"; ;
+        }
+        [HttpGet]
+      
+            public User GetUserId(int Id)
+            {
+                var user= users.FirstOrDefault(user => user.Id == Id);
+               
+                return user;
+            }
+        }
+    }
+
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication7.Models
+{
+    public class User
+    { 
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+         public string Password { get; set; }
+    }
+}
